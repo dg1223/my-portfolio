3d68113b9c0eeed1dfb8294009c5d91433cc70f6
dg1223 <alavi1223@hotmail.com>
2023-07-03 21:59:57 +0600

    feat: added dark mode toggle button to navbar

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,9 +1,29 @@
-import React from "react";
-
 const Banner = () => {
   return (
-    <div className="bg-gray-100 py-10">
-      {/* Your animation code here */}
+    <div className="flex items-center">
+      <div className="">
+        <h1 className="text-3xl lg:text-4xl font-semibold text-gray-800 dark:text-white">
+          Best Deal
+        </h1>
+        <p className="text-base lg:text-xl text-gray-800 dark:text-white mt-2">
+          Save upto <span className="font-bold">50%</span>
+        </p>
+      </div>
+      <div className="mt-8 md:mt-0">
+        <img
+          src="https://i.ibb.co/d7FrQrx/profile-photo-trimmed.png"
+          alt=""
+          className=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
+
+{
+  /* <div className="bg-gray-100 py-10">
       <div className="container mx-auto flex justify-between items-center">
         <div>
           <h1 className="text-4xl font-bold">Data Scientist</h1>
@@ -18,8 +38,5 @@ const Banner = () => {
           />
         </div>
       </div>
-    </div>
-  );
-};
-
-export default Banner;
+    </div> */
+}
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import ThemeToggle from "./ThemeToggle";
 
 const Navbar = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const handleToggle = () => {
+    setDarkMode(!darkMode);
+    // Toggle the dark theme using CSS classes or other methods
+  };
   return (
-    <nav className="bg-gray-800 text-white py-4 px-8 flex justify-between items-center">
+    <nav className="bg-gray-700 text-white py-4 px-8 flex justify-between items-center">
       <div className="flex items-center">
         <Link to="/" className="text-white font-bold mr-4">
           Home
         </Link>
+        <ThemeToggle darkMode={darkMode} handleToggle={handleToggle} />
       </div>
       <div className="hidden md:flex space-x-4">
         <Link to="/" className="text-gray-400 hover:text-white">
